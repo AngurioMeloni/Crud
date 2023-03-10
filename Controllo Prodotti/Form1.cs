572b9d4833b3fee194975e90db60b1e9667766cf
Angurio meloni <masserini.riccardo.studente@itispaleocapa.it>
2023-03-10 13:09:11 +0100

    modifica e riaggiunta delle funzioni presenti sullo scorso file

--- Controllo Prodotti/Form1.cs
+++ Controllo Prodotti/Form1.cs
@@ -32,7 +32,7 @@ namespace Controllo_Prodotti
         {
             InitializeComponent();
             dim = 0;
-            filename = @"carrello.csv";
+            filename = @"spesa.csv";
             prodotto.prod = new string[100];
             prodotto.prezzo = new string[100];
         }
@@ -40,111 +40,100 @@ namespace Controllo_Prodotti
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //chiamata alla funzione di caricamento, prendendo come parametri da mettere nello struct le stringhe presenti nelle textBox
             caricamento(textBox1.Text, textBox2.Text);
 
-            //chiamata alla funzione di stampa
+            
             stampa();
 
-            //MessageBox per avvisare l'utente del caricamento del prodotto inserito
             MessageBox.Show("Elementi caricati e stampati con successo");
 
-            //Svuotamento della textBox1 per poter inserire ulteriori prodotti
             textBox1.Text = "";
             textBox2.Text = "";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //chiamata ad una falsa funzione di ricerca che avvisa solamente l'utente dell'esistenza del prodotto inserita
-            falsesearch(textBox3.Text);
+            falsaricerca(textBox3.Text);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //chiamata ad una funzione di ricerca che ritorna la posizione di un prodotto inserito dall'utente
             int posizione = search(textBox3.Text);
-
-            //nel caso la posizione sia diversa -1, ovvero che il prodotto esiste, ...
             if (posizione != -1)
             {
-                //... richiamo la funzione di cancellamento ...
-                canc(posizione);
+                
+                cancellazione(posizione);
 
-                //..., stampo nuovamente senza l'elemento ...
+              
                 stampa();
 
-                //... e avviso l'utente del cancellamento del prodotto ...
                 MessageBox.Show("Elemento cancellato correttamente");
             }
-            else //... , altrimenti, ...
+            else
             {
-                //... avviso l'utente della non esistenza della stringa inserita
-                MessageBox.Show("L'elemento non esisteva, non è stato perciò possibile cancellarlo");
+                
+                MessageBox.Show("L'elemento non esiste");
             }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            //chiamata ad una funzione di ricerca che ritorna la posizione di un prodotto inserito dall'utente
+            
             int posizione = search(textBox3.Text);
 
-            //nel caso la posizione sia diversa -1, ovvero che il prodotto esiste, ...
+            
             if (posizione != -1)
             {
-                //... richiamo la funzione di modifica ...
-                mod(textBox4.Text, textBox5.Text, posizione);
+                
+                modifica(textBox4.Text, textBox5.Text, posizione);
 
-                //..., stampo nuovamente con l'elemento modificato ...
+                
                 stampa();
 
-                //... e avviso l'utente della modifica del prodotto ...
-                MessageBox.Show("Elemento modificato correttamente");
+               
+                MessageBox.Show("Elemento modificato");
             }
-            else //... , altrimenti, ...
+            else 
             {
-                //... avviso l'utente della non esistenza della stringa inserita
-                MessageBox.Show("L'elemento non esisteva, non è stato perciò possibile modificarlo");
+               
+                MessageBox.Show("L'elemento non esiste");
             }
-
-            //Svuotamento delle textBox per poter inserire ulteriori prodotti
             textBox4.Text = "";
             textBox5.Text = "";
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            calcp();
+            calcoloPrezzoTot();
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            //richiamo la funzione di sconto per calcolare il nuovo prezzo
+            
             sconto(int.Parse(textBox6.Text));
 
-            //richiamo la funzione di stampa per stampare i nuovi prezzi
+           
             stampa();
 
-            //pulisco la textbox per immetere un nuovo valore
+            
             textBox6.Text = "";
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            //richiamo la funzione di sconto per calcolare il nuovo prezzo
+            
             sconto(-int.Parse(textBox6.Text));
 
-            //richiamo la funzione di stampa per stampare i nuovi prezzi
+            
             stampa();
 
-            //pulisco la textbox per immetere un nuovo valore
+          
             textBox6.Text = "";
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            //richiamo alla funzione di creazione del file
-            create();
+            creazioneFile();
         }
         private void button10_Click(object sender, EventArgs e)
         {
@@ -154,46 +143,38 @@ namespace Controllo_Prodotti
         //funzione di caricamento
         static void caricamento(string p, string pr)
         {
-            //inserisco il nome e il prezzo del prosotto nello struct
+            
             prodotto.prod[dim] = p;
             prodotto.prezzo[dim] = pr;
-
-            //aumento la dimensione data l'aggiunta di elementi
             dim++;
         }
 
         //funzione di stampa
         void stampa()
         {
-            //pulisco la listview per poter ristampare gli array
             listView1.Items.Clear();
-
-            //stampa degli array nella listview attraverso un ciclo
             for (int i = 0; i < dim; i++)
             {
                 listView1.Items.Add(prodotto.prod[i] + " €" + prodotto.prezzo[i]);
             }
         }
 
-        //funzione di avviso di esistenza
-        void falsesearch(string nome)
+        //funzione di falsa ricerca
+        void falsaricerca(string nome)
         {
             //ciclo di ricerca sequenziale
             for (int i = 0; i < dim; i++)
             {
-                //nel caso venga trovato il prodotto ricercato ...
                 if (prodotto.prod[i] == nome)
                 {
-                    //... avviso l'utente e torno al programma
                     MessageBox.Show("Elemento trovato");
                     return;
                 }
             }
-            //nel caso non venga trovato nulla, avviso l'utente della non esistenza del prodotto
             MessageBox.Show("Elemento non trovato");
         }
 
-        //funzione di ricerca reale
+        //funzione di ricerca 
         int search(string nome)
         {
             //variabile che segna la posizione
@@ -201,57 +182,44 @@ namespace Controllo_Prodotti
 
             //ciclo di ricerca sequenziale
             for (int i = 0; i < dim; i++)
-            {
-                //nel caso venga trovato il prodotto ricercato ...
+            {              
                 if (prodotto.prod[i] == nome)
                 {
-                    //.. la variabile posizione prende il valore dell'indice del ciclo e ...
                     pos = i;
-
-                    //... ritorno il valore di posizione
                     return pos;
                 }
             }
-            //nel caso il prodotto non venga trovato, assegno il valore -1 alla variabile pos e ritrono al programma
             pos = -1;
             return pos;
         }
 
         //funzione di cancellamento
-        void canc(int pos)
+        void cancellazione(int pos)
         {
-            //ciclo di retrocessione degli elementi dalla posizione per il cancellamento
             for (int i = pos; i < dim; i++)
             {
                 prodotto.prezzo[i] = prodotto.prezzo[i + 1];
                 prodotto.prod[i] = prodotto.prod[i + 1];
             }
-
-            //diminuzione della grandezza degli array dato il cancellamento di uno degli elementi
             dim--;
         }
 
         //funzione di modifica
-        void mod(string nome, string prez, int pos)
+        void modifica(string nome, string prez, int pos)
         {
-            //modifica degli elementi nella posizione trovata dalla funzione di ricerca
             prodotto.prod[pos] = nome;
             prodotto.prezzo[pos] = prez;
         }
 
         //funzione di calcolo del prezzo totale
-        void calcp()
+        void calcoloPrezzoTot()
         {
-            //variabile locale per il calcolo del prezzo
-            float prezzo = 0;
 
-            //ciclo per il calcolo
+            float prezzo = 0;
             for (int i = 0; i < dim; i++)
             {
                 prezzo += float.Parse(prodotto.prezzo[i]);
             }
-
-            //aggiunta del prezzo alla listview
             listView1.Items.Add("prezzo spesa: €" + prezzo);
         }
 
@@ -260,21 +228,16 @@ namespace Controllo_Prodotti
         {
             for (int i = 0; i < dim; i++)
             {
-                //calcolo dello sconto su una variabile temporanea
                 float nuovop = float.Parse(prodotto.prezzo[i]) + (float.Parse(prodotto.prezzo[i]) / 100 * sconto);
-
-                //spostamento del valore della variabile sull'array apposito
                 prodotto.prezzo[i] = nuovop.ToString();
             }
         }
 
         //funzione di creazione del file
-        void create()
+        void creazioneFile()
         {
-            //creazione del file
             using (StreamWriter sw = new StreamWriter(filename, append: false))
             {
-                //ciclo di copia della listview sul file
                 for (int i = 0; i < dim; i++)
                 {
                     sw.WriteLine(prodotto.prod[i] + " €" + prodotto.prezzo[i]);
@@ -285,12 +248,12 @@ namespace Controllo_Prodotti
         //funzione di lettura del file
         void read()
         {
-            //lettura del file
+            
             using (StreamReader sr = File.OpenText(filename))
             {
                 string s;
 
-                //ciclo di stampa sulla listview
+                
                 while ((s = sr.ReadLine()) != null)
                 {
                     listView1.Items.Add(s);
